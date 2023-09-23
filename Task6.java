5206d5b1eb20e5fc53fd601360688f3654eeefeb
saajaanaa <sajnaa.shrestha@gmail.com>
2023-09-22 23:20:31 -0400

    modified task 6

--- Task6.java
+++ Task6.java
@@ -1,10 +1,30 @@
 package JavaOopsProject;
-/**Find the First Non-Repeating Character in a String: Given a string,
-        find and return the first non-repeating character. For example, in the
-        string "abracadabra", the first non-repeating character is 'c'.*/
+/**
+ * Find the First Non-Repeating Character in a String: Given a string,
+   find and return the first non-repeating character. For example, in the
+   string "abracadabra", the first non-repeating character is 'c'.*/
 
 
 public class Task6 {
+    public static void main(String[] args) {
+        String string = "abracadabra";
+        int index = -1;
+        char fnc = ' ';
 
-
+        for (char i : string.toCharArray()) {
+            if (string.indexOf(i) == string.lastIndexOf(i)) {
+                fnc = i;
+                break;
+            }
+            else {
+                index += 1;
+            }
+        }
+        if (index == string.length()-1) {
+            System.out.println("All characters are repeating");
+        }
+        else {
+            System.out.println("First non-repeating character: " + fnc);
+        }
+    }
 }
